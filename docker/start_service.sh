6e575fb09fc83abf4867bc8763ae09e387716e48
testuser770770 <testuser770770@gmail.com>
2019-09-04 19:57:08 +0300

    update

--- docker/start_service.sh
+++ docker/start_service.sh
@@ -1,5 +1,5 @@
 touch /root/docker.log
-echo "${root_password}" | passwd --stdin root
+echo "${rootpassword}" | passwd --stdin root
 /usr/sbin/sshd -D &  >> /root/docker.log
 /usr/sbin/httpd -D FOREGROUND & >> /root/docker.log
 tail -f /root/docker.log
